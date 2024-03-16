77d636e65b2e4e8afb33b57c30d8533062ac8b06
MSI\IT_SUPPCHAI <it.developer@sparsha.net>
2024-03-16 23:43:55 +0700

    presentation-db-mock

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/Max2535/simplebank/api"
+	db "github.com/Max2535/simplebank/db/sqlc"
+	"github.com/Max2535/simplebank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("connot start server:", err)
+	}
+
+}
